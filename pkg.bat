68176515ca7f6611072ba164fb117dba0f1ddcf6
luoxin <luoxin.ttt@gmail.com>
2020-06-11 14:46:28 +0800

    rename

--- pkg.bat
+++ pkg.bat
@@ -2,5 +2,5 @@ pip install virtualenv
 virtualenv venv
 venv\Scripts\pip.exe install -r requirements.txt
 venv\Scripts\pip.exe install pyinstaller
-venv\Scripts\pyinstaller.exe -F main.py --hiddenimport pkg_resources.py2_warn
+venv\Scripts\pyinstaller.exe -F update_hosts.py --hiddenimport pkg_resources.py2_warn
 del /F /S /Q venv
--- pkg.sh
+++ pkg.sh
@@ -2,5 +2,5 @@ pip install virtualenv
 virtualenv venv
 ./venv/bin/pip install -r ./requirements.txt
 ./venv/bin/pip install pyinstaller
-./venv/bin/pyinstaller -F ./main.py --hiddenimport pkg_resources.py2_warn
+./venv/bin/pyinstaller -F ./update_hosts.py --hiddenimport pkg_resources.py2_warn
 rm -rf ./venv
